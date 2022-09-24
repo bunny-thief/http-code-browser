2db3f318c620cabe7c8ea5b8113b18715b4be2f8
bunny-thief <4421229+bunny-thief@users.noreply.github.com>
2022-09-24 22:55:09 +0200

    Create ResponseCode model, superuser   - jnavarro, z15

--- code_browser/models.py
+++ code_browser/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class ResponseCode(models.Model):
+    response_code = models.CharField(max_length = 3)
+    #response_str = str(response_code)
+    description = models.TextField()
+    URL = models.URLField()
+    response_type = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.response_code
